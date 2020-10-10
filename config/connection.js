4e65633846cee863c2dcd9f1926adfa910cfd6ac
Erin Crocker <erindowns1@me.com>
2020-10-10 15:58:45 -0400

    updated connection for JAWSdb

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,20 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Bubbles92",
-    database: "burgers_db"
-  });
 
-// Connect to the database
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost' ,
+        user: 'root',
+        password: 'Bubbles92',
+        database: 'burgers_db'
+    });
+};
 
 // Export connection
+connection.connect();
 module.exports = connection;
